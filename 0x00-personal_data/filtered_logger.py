34307002947cbe53f2cfe610f0ef272cb90aa7ca
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-07 19:46:20 +0100

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -52,7 +52,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database"""
     config = {
         'host': os.getenv('PERSONAL_DATA_DB_HOST'),
