73e5dc5fe88b9a5facb21cd5b3e435eec372f37b
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-07 14:27:39 +0100

    0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+0. Regex-ing
+"""
+
+import re 
+
+
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]*{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
