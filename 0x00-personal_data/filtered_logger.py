079df90fd6838b23a498bdf4385e8765299e0ddb
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-07 15:51:13 +0100

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,16 @@
 from typing import List
 import re
 import logging
+import json
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]*{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
@@ -16,17 +26,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(f"{field}=[^{separator}]*{separator}",
-                         f"{field}={redaction}{separator}", message)
-    return message
+        log = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
