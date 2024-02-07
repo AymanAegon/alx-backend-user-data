46c5aa7a7bda610e447a2c40213807477ec16c98
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-07 14:36:40 +0100

    annotation fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,28 @@
 0. Regex-ing
 """
 
+from typing import List
 import re
+import logging
 
 
-def filter_datum(fields, redaction, message, separator):
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(f"{field}=[^{separator}]*{separator}",
