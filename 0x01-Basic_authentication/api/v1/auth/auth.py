205047b0729baf7da5a6dd3ba732dbf545d90cd2
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-12 18:58:59 +0100

    3 fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,17 +10,14 @@ from typing import List, TypeVar
 class Auth:
     """Auth Class"""
 
-    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returs False"""
         return False
-
 
     def authorization_header(self, request=None) -> str:
         """returns None"""
         return None
 
-
-    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """returns None"""
         return None
