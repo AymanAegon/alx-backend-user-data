ad3041afc80dc324e89ac5350f405a0fb24a3e6d
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-13 18:43:04 +0100

    9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,15 @@ class BasicAuth(Auth):
             return base64.b64decode(base64_authorization_header).decode()
         except Exception:
             return None
+        
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):  # type: ignore
+        """returns the user email and pwd from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':'))
