c943c0fed23c7147421167015b69089538d5e26b
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-14 15:33:41 +0100

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,14 +54,17 @@ def auth_check() -> str:
     else:
         request.current_user = auth.current_user(request)
         excluded_paths = ['/api/v1/status/',
-                          '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/'
+                          ]
         if auth.require_auth(request.path, excluded_paths) is False:
             return
-        if auth.authorization_header(request) is None:
+        auth_header = auth.authorization_header(request)
+        if auth_header is None and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
-
 
 
 if __name__ == "__main__":
