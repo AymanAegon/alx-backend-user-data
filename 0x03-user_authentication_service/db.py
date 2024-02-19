64a2f4463c7cb02d2076235ff1c26471bef36382
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-19 17:57:04 +0100

    2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
 
@@ -46,13 +46,8 @@ class DB:
         takes in arbitrary keyword arguments and returns the first row
         found in the users table as filtered by the method's input arguments
         """
-        fields = []
-        values = []
         for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
+            if not hasattr(User, key):
                 raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
