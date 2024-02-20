7db99b8b5cd94ef99019584a353e758c931bce20
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-20 19:21:47 +0100

    9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,6 +16,11 @@ def _hash_password(password: str) -> bytes:
     return hashpw(password.encode("utf-8"), gensalt())
 
 
+def _generate_uuid() -> str:
+    """return a string representation of a new UUID"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -44,10 +49,6 @@ class Auth:
             return True
         return False
 
-    def _generate_uuid(self) -> str:
-        """return a string representation of a new UUID"""
-        return str(uuid.uuid4())
-
     def create_session(self, email: str) -> str:
         """takes an email string argument and
         returns the session ID as a string"""
@@ -55,6 +56,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return None
-        session_id = self._generate_uuid()
+        session_id = _generate_uuid()
         self._db.update_user(user_id=user.id, session_id=session_id)
         return session_id
