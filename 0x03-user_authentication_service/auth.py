e00e85db4f7a113dcac4f26cf14496271d35b543
Ayman Ammar <ayman24ammar@gmail.com>
2024-02-21 12:31:13 +0100

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,7 +84,7 @@ class Auth:
         """
         user = self._db.find_user_by(email=email)
         if user is None:
-            raise ValueError
+            raise ValueError()
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
